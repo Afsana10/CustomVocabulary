5465f0f62e1388ce24db24ce1f3c754fd927fc55
Afsana10 <kaziafsana373@gmail.com>
2025-05-15 00:51:39 +0700

    created data model; saved and showed data

--- CustomVocabularyApp/models.py
+++ CustomVocabularyApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Vocabulary(models.Model):
+    word = models.CharField(max_length=100, unique=True)  # ✅ For short text, like a single word
+    meaning = models.TextField()  # ✅ For longer text, like multiple sentences
--- CustomVocabularyApp/views.py
+++ CustomVocabularyApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from .models import Vocabulary
+from django.shortcuts import redirect 
 
 # Create your views here.
 from django.http import HttpResponse
 
 def first(request):
+   if request.method == 'POST':
+       # Handle the form submission
+       word = request.POST.get('englishWordTextbox')
+       meaning = request.POST.get('bengaliMeaningTextbox')
+       if word and meaning:
+            Vocabulary.objects.create(word=word, meaning=meaning)
+   vocab_list = Vocabulary.objects.all()
+       # Here you would typically save the data to the database
+       # For example: Vocabulary.objects.create(word=word, meaning=meaning)
+       # return HttpResponse(f"Word: {word}, Meaning: {meaning}")
    # return HttpResponse("Hello, world!")
-   return render(request, 'CustomVocabularyApp/index.html')
+   return render(request, 'CustomVocabularyApp/index.html',{"vocab_list": vocab_list})
